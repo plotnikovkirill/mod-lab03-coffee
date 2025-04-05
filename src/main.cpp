b203e005bd771cf7199033134df72b1ee4911770
plotnikovkirill <82108541+plotnikovkirill@users.noreply.github.com>
2025-04-05 21:07:15 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// Copyright 2022 UNN-IASR
+#include "Automata.h"
+
+int main() {
+	Automata machine = Automata();
+	machine.on();
+	machine.getMenu();
+	machine.coin(120);
+	machine.coin(10);
+	machine.choice("capuchino");
+}
