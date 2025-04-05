a4a5b0295c7f2310c672ee0718dcac85b5be0396
plotnikovkirill <82108541+plotnikovkirill@users.noreply.github.com>
2025-04-05 21:29:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include "Automata.h"
 
 int main() {
-	Automata machine = Automata();
-	machine.on();
-	machine.getMenu();
-	machine.coin(120);
-	machine.coin(10);
-	machine.choice("capuchino");
+  Automata machine = Automata();
+  machine.on();
+  machine.getMenu();
+  machine.coin(120);
+  machine.coin(10);
+  machine.choice("capuchino");
 }
